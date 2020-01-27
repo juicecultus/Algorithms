7177b8cf21e5ce9880006c72087ca5b99189fed3
J B <justinian.boghici@gmail.com>
2020-01-27 12:11:25 +0000

    Reverse Integer Exercise - Add Problem & Tests

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,19 +2,18 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split('').reduce((rev, char) => char + rev, '');
+  return str.split("").reduce((rev, char) => char + rev, "");
 }
 
 module.exports = reverse;
 
-
 // OTHER POSSIBLE SOLUTIONS:
-// 
+//
 // function reverse(str) {
 //     return str
 //         .split('')
@@ -24,7 +23,7 @@ module.exports = reverse;
 
 // function reverse(str) {
 //     let reversed = '';
-//  
+//
 //     for (let character of str) {
 //         reversed = character + reversed;
 //     }
